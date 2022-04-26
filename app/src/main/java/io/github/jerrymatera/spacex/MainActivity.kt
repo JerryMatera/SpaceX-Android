90d0e681ae2d58f29657aaaf63beb69f66bcab36
JerryMatera <jerryonyango.95@gmail.com>
2022-04-26 04:33:18 +0300

    Setup App's navigation

--- app/src/main/java/io/github/jerrymatera/spacex/MainActivity.kt
+++ app/src/main/java/io/github/jerrymatera/spacex/MainActivity.kt
@@ -2,10 +2,14 @@ package io.github.jerrymatera.spacex
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import io.github.jerrymatera.spacex.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
